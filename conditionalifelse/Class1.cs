fb4c464bc477329cfb1a5457670336ef1cff66dc
Bhaktikanchan532 <kanchanbhakti@gmail.com>
2022-09-30 17:11:55 +0530

    leapoddeven

---
+++ conditionalifelse/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conditionalifelse
+{
+    internal class Class1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Year");
+
+            int year = Convert.ToInt32(Console.ReadLine());
+            if(year%4==0)
+            {
+                Console.WriteLine(" Leap Year");
+            }
+            else
+            {
+                Console.WriteLine(" Not Leap Year");
+            }
+        }
+
+    }
+}
---
+++ conditionalifelse/Program.cs
@@ -0,0 +1,26 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace conditionalifelse
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("Enter Any Number");
+//            int n = Convert.ToInt32(Console.ReadLine());
+//            if (n % 2 == 0)
+//            {
+//                Console.WriteLine("Even Number");
+//            }
+//            else
+//            {
+//                Console.WriteLine("Odd Number");
+//            }
+            
+//        }
+//    }
+//}
